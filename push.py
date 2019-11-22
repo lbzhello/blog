7f4fc0819278ca251ff662609cbc0371eeea7d5c
lbzhello <lbzhello@qq.com>
2019-11-22 19:02:59 +0800

    python push test

---
+++ push.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import sys
+import os
+
+msg = 'update'
+
+# 第一个参数为消息内容
+if len(sys.argv) == 2:
+     msg = sys.argv[1]
+
+ad = 'git add .'
+co = 'git commit -am "{}"'.format(msg)
+pu = 'git push origin master'
+
+os.system(ad)
+os.system(co)
+os.system(pu)
